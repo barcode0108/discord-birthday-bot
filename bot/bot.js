b5bf61d75a21a29a66c24733b0416140c41835aa
Barcode <barcode0108@gmail.com>
2019-07-16 00:19:23 +0800

    test

--- bot/bot.js
+++ bot/bot.js
@@ -2,7 +2,6 @@ const config = require('../config');
 const discord = require('discord.js');
 
 const client = new discord.Client();
-const message = require('./message');
 
 
 let isInit = false;
@@ -11,14 +10,17 @@ let isInit = false;
 module.exports = {
   init: () => {
     if (isInit) return;
-
+    
     client.on('ready', () => {
       console.log('Bot is ready!')
     });
-
+    
     client.login(config.discord_token);
-
+    
     isInit = true;
   },
   bot: client
 }
+
+
+const message = require('./message');
