5c61f8e757d76b18e881120ab958f0161791584f
Barcode <barcode0108@gmail.com>
2019-07-16 00:32:47 +0800

    test

--- bot/reaction.js
+++ bot/reaction.js
@@ -12,7 +12,7 @@ const getLastSentMessage = async channel => {
   const messages = await channel.fetchMessages({ limit: 1 });
   const lastMessage = messages.first();
 
-  if (!lastMessage.author.bot) {}
+  if (!lastMessage.author.bot) { }
 
   return lastMessage;
 }
@@ -26,28 +26,31 @@ const comfirmLoginToBlizzard = async (client, channel_id) => {
 
   const message = await getLastSentMessage(channel);
 
-  message.react(emojiObj.check).then(() => message.react(emojiObj.cross));
 
   const filter = (reaction, user) => {
     return [emojiObj.check, emojiObj.cross].includes(reaction.emoji.name) && user.id === message.author.id;
   };
-  
-  message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
-    .then(collected => {
-      const reaction = collected.first();
 
-      if (reaction.emoji.name === emojiObj.check) {
-        message.channel.send('V');
-      } else {
-        message.channel.send('X');
-      }
-      message.delete();
-    })
-    .catch(collected => {
-      message.channel.send('NULL');
-      message.delete();
+  message.react(emojiObj.check).then(() => message.react(emojiObj.cross))
+    .then(() => {
+
+      message.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
+        .then(collected => {
+          const reaction = collected.first();
+
+          if (reaction.emoji.name === emojiObj.check) {
+            message.channel.send('V');
+          } else {
+            message.channel.send('X');
+          }
+          message.delete();
+        })
+        .catch(collected => {
+          message.channel.send('NULL');
+          message.delete();
+        });
+
     });
-
 }
 
 
